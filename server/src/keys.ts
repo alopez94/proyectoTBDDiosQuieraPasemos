2b09f3a5bd312aa0f3ddbb430a877c1895d6efd6
Allan <allan.u94l@gmail.com>
2020-06-30 17:48:11 -0600

    commit 3

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -1,10 +1,19 @@
-export default {
+export default  {
 
     database: {
         host: 'localhost',
         user: 'root',
         password: '',
         database: 'ProjectoTBD'
-    }
+    },
 
+    databaseSQL: {
+        
+        user: 'sa1',
+        password: 'abcd1234**',
+        server: 'DESKTOP-TD3NOBK\SQLSERVERPROJTUR', 
+        database: 'proyectotbd'
+        
+        
+    }
 }
